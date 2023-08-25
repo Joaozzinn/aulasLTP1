4d09f01814085a4ac7dea711edcf3d9dfd14bd7d
Joazzinn <jfsantos@aluno.riobrancofac.edu.br>
2023-08-24 22:49:07 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,14 @@
+//Console.log eu uso para
+// mostrar uma mensagem ao usuário
+console.log("Hello");
+
+//alert exibe uma mensagem de alerta
+//ao usuário
+alert("Hello!");
+
+
+//Para declarar uma variável no JS
+//podemos usar "var", "let" ou "const"
+const pi = 3.14
+
+alert(pi)
